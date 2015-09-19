cf1d95db12e5110ce0936f0b53ce69f04fb8f933
Fredrik Sjöberg <fredrik.sjoberg@knightsfee.com>
2015-09-19 15:49:41 +0200

    ContextType

---
+++ SnapStack/Protocols/ContextType.swift
@@ -0,0 +1,44 @@
+//
+//  ContextType.swift
+//  SnapStack
+//
+//  Created by Fredrik Sjöberg on 2015-09-19.
+//  Copyright © 2015 Fredrik Sjoberg. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public protocol ContextType {
+    var handlerContext: NSManagedObjectContext { get }
+    
+    func fetch<T: NSManagedObject>(entity: T.Type) -> QuerySet<T>
+    func create<T: NSManagedObject>(entity: T.Type) -> T?
+    func edit<T: NSManagedObject>(entity: T?) -> T?
+    func delete<T: NSManagedObject>(entity: T)
+    
+    func commit() throws
+}
+
+public extension ContextType {
+    func fetch<T: NSManagedObject>(entity: T.Type) -> QuerySet<T> {
+        return QuerySet<T>(handlerContext, T.entityName())
+    }
+    
+    public func create<T: NSManagedObject>(entity: T.Type) -> T? {
+        return NSEntityDescription.insertNewObjectForEntityForName(entity.entityName(), inManagedObjectContext: handlerContext) as? T
+    }
+    
+    public func edit<T: NSManagedObject>(entity: T?) -> T? {
+        return entity?.inContext(handlerContext)
+    }
+    
+    public func delete<T: NSManagedObject>(entity: T) {
+        entity.inContext(handlerContext)?.deleteFromContext()
+    }
+    
+    public func commit() throws {
+        guard handlerContext.hasChanges else { return }
+        try handlerContext.save()
+    }
+}
