f44a28e6ac96002db25dbb4754c37731b5bcfdab
Fredrik Sjöberg <fredrik.sjoberg@knightsfee.com>
2015-09-19 15:35:12 +0200

    Initial Commit

---
+++ SnapStack/SnapStack.h
@@ -0,0 +1,19 @@
+//
+//  SnapStack.h
+//  SnapStack
+//
+//  Created by Fredrik Sjöberg on 2015-09-19.
+//  Copyright © 2015 Fredrik Sjöberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnapStack.
+FOUNDATION_EXPORT double SnapStackVersionNumber;
+
+//! Project version string for SnapStack.
+FOUNDATION_EXPORT const unsigned char SnapStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapStack/PublicHeader.h>
+
+
