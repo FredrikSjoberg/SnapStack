bb4679f008bab8bfd3776e1e638fe70cb9e62ddc
Fredrik Sjöberg <fredrik.sjoberg@knightsfee.com>
2015-09-19 15:49:08 +0200

    Extensions to QuerySet

---
+++ SnapStack/Extensions/QuerySet+Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  QuerySet+Extensions.swift
+//  SnapStack
+//
+//  Created by Fredrik Sjöberg on 2015-09-17.
+//  Copyright © 2015 Fredrik Sjoberg. All rights reserved.
+//
+
+import Foundation
+
+
+public extension QuerySet {
+    public func unique() throws -> ModelType? {
+        let result = try array()
+        guard result.count == 1 else { return nil }
+        return result.first
+        /*
+        do {
+            let result = try array()
+            guard result.count == 1 else { return nil }
+            return result.first
+        }
+        catch {
+            return nil
+        }
+        */
+    }
+}
