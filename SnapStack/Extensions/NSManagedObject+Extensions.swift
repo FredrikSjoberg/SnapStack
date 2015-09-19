f23e0b3b95a5f1044a52a433a3d8b5f71accc9e6
Fredrik Sjöberg <fredrik.sjoberg@knightsfee.com>
2015-09-19 15:48:48 +0200

    Extensions to NSManagedObject

---
+++ SnapStack/Extensions/NSManagedObject+Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  NSManagedObject+Extensions.swift
+//  SnapStack
+//
+//  Created by Fredrik Sjöberg on 2015-09-17.
+//  Copyright © 2015 Fredrik Sjoberg. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+internal extension NSManagedObject {
+    internal class func entityName() -> String {
+        return  NSStringFromClass(self).componentsSeparatedByString(".").last!
+    }
+}
+
+internal extension NSManagedObject {
+    internal func inContext<T: NSManagedObject>(context: NSManagedObjectContext) -> T? {
+        if self.managedObjectContext === context { return self as? T }
+        
+        if objectID.temporaryID {
+            do {
+                try withExtendedLifetime(managedObjectContext) {
+                    return try managedObjectContext?.obtainPermanentIDsForObjects([self])
+                }
+            }
+            catch let nsError as NSError {
+                print("NSManagedObject.inContext(): \(nsError)")
+                return nil
+            }
+        }
+        
+        do {
+            return try context.existingObjectWithID(objectID) as? T
+        }
+        catch let nsError as NSError {
+            print("NSManagedObject.inContext(): \(nsError)")
+            return nil
+        }
+    }
+}
+
+public extension NSManagedObject {
+    public func deleteFromContext() {
+        managedObjectContext?.deleteObject(self)
+    }
+}
